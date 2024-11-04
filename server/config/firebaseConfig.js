853e50727b0e43a6f9d8f374178bd939b14f87a1
revanth1718 <revanthpasupuleti18@gmail.com>
2024-11-04 23:14:53 +0530

    again changed

--- server/config/firebaseConfig.js
+++ server/config/firebaseConfig.js
@@ -1,12 +1,15 @@
 import admin from 'firebase-admin';
 
-const initializeFirebase = async () => {
+export const initializeFirebase = async () => {
     const serviceAccountKey = await import("../prepinsights-523c5-firebase-adminsdk-7wqhl-4b30957308.json", {
         assert: { type: "json" }
     });
+    
     admin.initializeApp({
         credential: admin.credential.cert(serviceAccountKey.default),
     });
 };
 
-export { initializeFirebase, admin as getFirebaseAdmin };
+export const getFirebaseAdmin = () => {
+    return admin;
+};
